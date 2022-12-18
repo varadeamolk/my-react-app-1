455e2a7c5f8080e73a07f4e1b096b65166245958
santoshmondal <santosh.ece06@gmail.com>
2022-12-18 11:24:35 +0530

    JSX is the hero!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+// JSX!
+// JSX => JAVA SCRIPT + XML
+// XML => Extended the HTML and Cratedad new Tag
+// App => <App />
 function App() {
-  return "Hello!";
+  return <h1>Hello</h1>;
 }
+
+/*function normalFn() {
+  // ..
+  // ..
+  // ..
+  return 100;
+  return "abcd"
+  return true;
+  return [];
+  return {};
+}*/
 
 export default App;
