d558f3b9ee3184da54b02d5b8d5d8f218f118564
santoshmondal <santosh.ece06@gmail.com>
2022-12-18 11:50:57 +0530

    multiple jsx.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-// Multi Tag
 function App() {
   return (
     <div>
-      <h1>Div and H1</h1>
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+    </div>
+  );
+}
+
+// <Person />
+function Person() {
+  return (
+    <div>
+      <img src="https://picsum.photos/400" alt="" />
+      <h1>Person Name </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
+        delectus placeat? Consequuntur iusto perferendis voluptatibus, ipsa sit
+        non. Quo cumque veniam nostrum voluptatem qui accusantium ipsa labore
+        libero! Tempore, consequuntur!
+      </p>
     </div>
   );
 }
