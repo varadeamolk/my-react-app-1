a0fb42b794d55b74ea987fa8dc803583032db2f8
santoshmondal <santosh.ece06@gmail.com>
2022-12-18 11:33:24 +0530

    jsx - multi line statement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-// SINGLE LINE JSX
+// MULTI LINE JSX ::: <App />
 function App() {
-  return <h1>Hello World!!!</h1>;
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sed maxime
+      quisquam inventore reprehenderit! Nobis ducimus, voluptatem cum at
+      placeat, quia facere ut natus ipsam id temporibus iusto velit officia?
+    </h1>
+  );
 }
 
 export default App;
