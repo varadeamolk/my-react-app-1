8e850e81c7bd86c8e7be452ebdd2824683001933
santoshmondal <santosh.ece06@gmail.com>
2022-12-18 11:54:36 +0530

    jsx.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 function App() {
   return (
     <div>
-      <Person />
-      <Person />
-      <Person />
-      <Person />
-      <Person />
-      <Person />
-      <Person />
+      <Person name="Rahul" />
+      <Person name="Rohit" />
+      <Person name="Kharghar" />
+      <Person name="Belapur" />
+      <Person name="Nerul" />
+      <Person name="Panvel" />
+      <Person name="Lonavala" />
     </div>
   );
 }
 
 // <Person />
-function Person() {
+function Person({ name }) {
   return (
     <div>
       <img src="https://picsum.photos/400" alt="" />
-      <h1>Person Name </h1>
+      <h1>Person Name : {name} </h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
         delectus placeat? Consequuntur iusto perferendis voluptatibus, ipsa sit
