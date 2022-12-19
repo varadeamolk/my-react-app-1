08e598ecf32320ec5a5a72966816bedd4a513402
santoshmondal <santosh.ece06@gmail.com>
2022-12-19 11:56:32 +0530

    stateful member.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
+import { useState } from "react";
+
 function App() {
   let str1 = "Home";
   let str2 = "Explore";
   let str3 = "Notifications";
-  let counter = 1;
+  // let counter = 1;
+  let [counter, setCounter] = useState(1);
+
+  // let city = "mumbai";
+  let [city, setCity] = useState("mumbai");
+  let changeCity = () => {
+    city = "Hello " + city;
+
+    // RE-RENDERING THE city value.
+    setCity(city);
+  };
 
   let increment = () => {
+    // logical part
     counter++;
     console.log(counter);
+
+    // DOM Update Part.
+    setCounter(counter);
   };
 
   return (
     <div>
+      <h1>{city}</h1>
+      <input type="button" value="Change City" onClick={changeCity} />
+      <hr />
       <h1>{str1}</h1>
       <h1>{str2}</h1>
       <h1>{str3}</h1>
