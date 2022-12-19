65f581464662ef5fe6d094951a62c5e605c646e6
santoshmondal <santosh.ece06@gmail.com>
2022-12-19 12:26:21 +0530

    map1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 function App() {
   let title = "Map Demo";
-  let user = { id: 1, city: "mumbai" };
+  let list = ["mumbai", "delhi"];
 
   return (
     <div>
       <h1> {title} </h1>
-      <h1>{user.city}</h1>
+
+      {list.map((item) => (
+        <h1>{item}</h1>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+// [delhi, mumbai] => [<h1>delhi</h1>, <h1>mumbai</h1>]
