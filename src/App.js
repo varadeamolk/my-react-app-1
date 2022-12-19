0d55bbf6b36dbee0810e1b331fc06371164ec620
santoshmondal <santosh.ece06@gmail.com>
2022-12-19 10:56:31 +0530

    basic inter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // USER DEFINED TAGS == COMPONENT
 function App() {
-  // PURE DATA MEMBERS
   let project = "My React Project";
   let username = "rahul";
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <div>
       <h1> {project} </h1>
-      <h1>Hello {username} !</h1>
       <h1>Hello {username} !</h1>
     </div>
   );
