23ef8b648cbbbf3fcfa695f0f43f921e5852e63d
santoshmondal <santosh.ece06@gmail.com>
2022-12-19 11:34:06 +0530

    stateful counter.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
+
 function App() {
-  // stateless variable
-  let counter = 100;
+  // let counter = 10;
+  let [counter, setCounter] = useState(10);
 
   let increment = () => {
     counter = counter + 1;
-    console.log(counter);
+
+    // RE-RENDER
+    setCounter(counter);
   };
 
   return (
