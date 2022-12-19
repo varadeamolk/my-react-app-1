e9251d8215304d11659c5efe561b6712a9e8208a
santoshmondal <santosh.ece06@gmail.com>
2022-12-19 11:16:17 +0530

    member function, how to use

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 function App() {
+  // Data Members
   let project = "Counter Application";
   let btnValue = "Click Meeeeeee";
 
+  // Member :: Type is Functions
+  let clickMe = () => alert();
+  let incrment = () => {};
+  let decrement = () => {};
+
+  // JSX
   return (
     <div>
       <h1> {project} </h1>
-      <input type="button" value={btnValue} />
+      <h1> </h1>
+      <input type="button" value={btnValue} onClick={clickMe} />
+
+      <input type="button" value="Increment" onClick={incrment} />
     </div>
   );
 }
