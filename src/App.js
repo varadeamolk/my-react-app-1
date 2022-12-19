8cb94f327e65f3b97196e100c0e576aff8faf9fe
santoshmondal <santosh.ece06@gmail.com>
2022-12-19 11:23:30 +0530

    counter statelsss

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 function App() {
-  // Data Members
-  let project = "Counter Application";
-  let btnValue = "Click Meeeeeee";
+  // stateless variable
+  let counter = 100;
 
-  // Member :: Type is Functions
-  let clickMe = () => alert();
-  let incrment = () => {};
-  let decrement = () => {};
+  let increment = () => {
+    counter = counter + 1;
+    console.log(counter);
+  };
 
-  // JSX
   return (
     <div>
-      <h1> {project} </h1>
-      <h1> </h1>
-      <input type="button" value={btnValue} onClick={clickMe} />
+      <h1>Counter Application</h1>
 
-      <input type="button" value="Increment" onClick={incrment} />
+      <h1>{counter}</h1>
+
+      <input type="button" value="Increment" onClick={increment} />
     </div>
   );
 }
