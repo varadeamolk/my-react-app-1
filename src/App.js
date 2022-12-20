f55d8d0fa182a1a915542b2a7d16147f164a6acf
santoshmondal <santosh.ece06@gmail.com>
2022-12-20 12:00:42 +0530

    map fn. key prop.

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
     <div>
       <h1 className="bg-primary text-white p-3">Messaging Demo</h1>
 
-      {list.map((item) => (
-        <div className="d-flex my-1">
+      {list.map((item, index) => (
+        <div key={index} className="d-flex my-1">
           <div className="badge text-bg-primary">
             {item.message}
             <span className="ms-4">
