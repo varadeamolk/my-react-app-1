12028bfc3c2a3c31f9d3322f331d70e1ead23d54
santoshmondal <santosh.ece06@gmail.com>
2022-12-20 09:56:00 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ function App() {
   return (
     <div>
       <h1 className="bg-dark text-light p-3">Bootstrap Styling!</h1>
+
+      <h1 className="bg-success" style={{ color: "#fff" }}>
+        hello
+      </h1>
     </div>
   );
 }
