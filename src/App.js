1457b8eecc6e3babc4f935e3770d6a118a6c7541
santoshmondal <santosh.ece06@gmail.com>
2022-12-20 20:39:32 +0530

    basics.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Link, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <>
+        <Link to={"/"}>Home | </Link>
+        <Link to={"/p1"}>P1 | </Link>
+        <Link to={"/p2"}>P2 | </Link>
+        <Link to={"/p3"}>P3 |</Link>
+      </>
+
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+        <Route path="/p1" element={<h1>P1</h1>} />
+        <Route path="/p2" element={<h1>P2</h1>} />
+        <Route path="/p3" element={<h1>P3</h1>} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </>
   );
 }
 
