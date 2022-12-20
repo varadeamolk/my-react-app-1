47f61674b9bdf6827936e098e9ca378dfba1256d
santoshmondal <santosh.ece06@gmail.com>
2022-12-20 11:31:06 +0530

    caputre the input.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,26 @@
+import { useState } from "react";
+
 function App() {
-  let clickMe1 = () => {
-    console.log("Hello");
-  };
+  let [message, setMessage] = useState("hello");
 
-  let clickMe2 = (e) => {
-    console.log(e);
-  };
-
-  // Custom Parameter :: using arrow funcn
-  let clickMe3 = (p1) => {
-    console.log(p1);
-  };
-
-  let clickMe4 = (e, p1) => {
-    console.log(e, p1);
+  let updateMessage = (e) => {
+    // e.target == document.querySelector("input")
+    setMessage(e.target.value);
   };
 
   return (
     <div>
-      <h1>Btn Click Demo</h1>
+      <h1>Working with Input</h1>
 
-      {/** Default No param */}
-      <input type="button" value="Click Me 1" onClick={clickMe1} />
-
-      {/** Default Event Param */}
-      <input type="button" value="Click Me 2" onClick={clickMe2} />
-
-      {/** Custom Param */}
       <input
-        type="button"
-        value="Click Me 3"
-        onClick={() => clickMe3("primary")}
+        type="text"
+        placeholder="Enter message"
+        value={message}
+        onChange={updateMessage}
       />
-      <input
-        type="button"
-        value="Click Me 3"
-        onClick={() => clickMe3("danger")}
-      />
+      <input type="button" value="Add Message" />
 
-      {/** Event + Custom Param */}
-      <input
-        type="button"
-        value="Click Me 4"
-        onClick={(e) => clickMe4(e, "adsfaf adsf")}
-      />
+      <h1>{message}</h1>
     </div>
   );
 }
