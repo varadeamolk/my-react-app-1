3b27f1b16c369a5c9325e930d0b2e558215051a0
santoshmondal <santosh.ece06@gmail.com>
2022-12-20 11:57:11 +0530

    map fn demo using, arry of object!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import { useState } from "react";
 
 function App() {
-  let [list] = useState(["delhi", "calcutta", "chennai"]);
+  let [list] = useState([
+    { message: "Hi", messageTime: new Date() },
+    { message: "How r u", messageTime: new Date() },
+    { message: "Good", messageTime: new Date() },
+    { message: "fine bro", messageTime: new Date() },
+  ]);
 
   return (
     <div>
-      <h1>Map Demo</h1>
+      <h1 className="bg-primary text-white p-3">Messaging Demo</h1>
 
       {list.map((item) => (
-        <div className="alert alert-primary">{item}</div>
+        <div className="d-flex my-1">
+          <div className="badge text-bg-primary">
+            {item.message}
+            <span className="ms-4">
+              {item.messageTime.getHours()}:{item.messageTime.getMinutes()}
+            </span>
+          </div>
+        </div>
       ))}
     </div>
   );
