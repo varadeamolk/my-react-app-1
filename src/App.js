31dd8a84d6daecaaed33d60c03737f5e202878ec
santoshmondal <santosh.ece06@gmail.com>
2022-12-22 12:03:07 +0530

    reading input from the user.

--- src/App.js
+++ src/App.js
@@ -16,13 +16,26 @@ function Counter1() {
   let dispatch = useDispatch();
   let { userAccount } = useSelector((state) => state);
 
+  let [amount, setAmount] = useState(0);
+  let handleInputChange = (e) => {
+    amount = parseInt(e.target.value);
+    setAmount(amount);
+  };
+
   return (
     <div>
       <h1>Counter 1 - Amount {userAccount.amount}</h1>
+
+      <input
+        type="number"
+        placeholder="Enter Amount"
+        value={amount}
+        onChange={handleInputChange}
+      />
       <input
         type="button"
         value="Deposit"
-        onClick={() => dispatch(deposit(200))}
+        onClick={() => dispatch(deposit(amount))}
       />
     </div>
   );
@@ -32,19 +45,31 @@ function Counter2() {
   let dispatch = useDispatch();
   let { userAccount } = useSelector((state) => state);
 
+  let [amount, setAmount] = useState(0);
+  let handleInputChange = (e) => {
+    amount = parseInt(e.target.value);
+    setAmount(amount);
+  };
+
   return (
     <div>
       <h1>Counter 2 - Amount {userAccount.amount}</h1>
+
+      <input
+        type="number"
+        placeholder="Enter amount"
+        onChange={handleInputChange}
+      />
       <input
         type="button"
         value="Deposit"
-        onClick={() => dispatch(deposit(100))}
+        onClick={() => dispatch(deposit(amount))}
       />
 
       <input
         type="button"
         value="Withdraw"
-        onClick={() => dispatch(withdraw(50))}
+        onClick={() => dispatch(withdraw(amount))}
       />
     </div>
   );
