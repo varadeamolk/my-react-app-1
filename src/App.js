8034ac23d5fc0c01f6ebdfe8193ccca64372bcac
Amol K V <varadeamolk.1994@gmail.com>
2022-12-23 17:33:32 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import { Link, Route, Routes } from "react-router-dom";
-import AppNavLinks from "./components/AppNavLinks";
-import Explore from "./components/Explore";
-import Home from "./components/Home";
-import Notifications from "./components/Notifications";
-import PageNotFound from "./components/PageNotFound";
-
 function App() {
   return (
     <div>
-      <AppNavLinks />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <h1>Hello</h1>
     </div>
   );
 }
-
 export default App;
