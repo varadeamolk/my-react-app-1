b4409c0d48339e70d1504f16434f08fdcfa7e07e
santoshmondal <santosh.ece06@gmail.com>
2022-12-23 11:10:15 +0530

    basic layout.

--- src/App.js
+++ src/App.js
@@ -14,29 +14,14 @@ function App() {
       element: <RootLayout />,
       children: [
         { index: true, element: <Page1 /> },
-        {
-          path: "login",
-          element: (
-            <UnProtectedRoute>
-              <Login />
-            </UnProtectedRoute>
-          ),
-        },
+
         {
           path: "page2",
-          element: (
-            <ProtectedRoute>
-              <Page2 />
-            </ProtectedRoute>
-          ),
+          element: <Page2 />,
         },
         {
           path: "page3",
-          element: (
-            <ProtectedRoute>
-              <Page3 />
-            </ProtectedRoute>
-          ),
+          element: <Page3 />,
         },
       ],
     },
@@ -44,69 +29,19 @@ function App() {
   return <RouterProvider router={router} />;
 }
 
-function ProtectedRoute({ children }) {
-  if (!localStorage.getItem("jwt")) {
-    return <Navigate to={"/login"} replace={true} />;
-  }
-
-  return children;
-}
-
-function UnProtectedRoute({ children }) {
-  if (localStorage.getItem("jwt")) {
-    return <Navigate to={"/"} replace={true} />;
-  }
-
-  return children;
-}
-
 function RootLayout() {
-  let jwt = localStorage.getItem("jwt");
-
-  if (!jwt) {
-    return (
-      <>
-        <Link to="/login" className="fs-3">
-          Login |
-        </Link>
-        <Link to="/" className="fs-3">
-          Page1 |
-        </Link>
-
-        <Outlet />
-      </>
-    );
-  } else {
-    return (
-      <div>
-        <Link to="/" className="fs-3">
-          Page1 |
-        </Link>
-        <Link to="/page2" className="fs-3">
-          Page2 |
-        </Link>
-        <Link to="/page3" className="fs-3">
-          Page3 |
-        </Link>
-
-        <Outlet />
-      </div>
-    );
-  }
-}
-
-function Login() {
-  let navigate = useNavigate();
-
-  let loginAction = () => {
-    localStorage.setItem("jwt", "true");
-    navigate(0);
-  };
   return (
-    <div>
-      <h1>Login</h1>
-      <input type="button" value="Login" onClick={loginAction} />
-    </div>
+    <>
+      <Link to="/" className="fs-3">
+        Page1 |
+      </Link>
+      <Link to="/page2" className="fs-3">
+        Page2 |
+      </Link>
+      <Link to="/page3" className="fs-3">
+        Page3 |
+      </Link>
+    </>
   );
 }
 
